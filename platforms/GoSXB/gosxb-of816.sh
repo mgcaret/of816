a8cde5abd99b4f300d971a4e7b6eda7dcfc73f80
mgcaret <mgcaret@users.noreply.github.com>
2020-10-29 23:20:03 -0700

    gosxb updates

--- platforms/GoSXB/gosxb-of816.sh
+++ platforms/GoSXB/gosxb-of816.sh
@@ -2,7 +2,7 @@
 EXEBIN=${GOSXB:-gosxb}
 cd `dirname $0`
 ROMFSOPTS=
-if [ -r romfs ]; then
+if [ -z "$1" ] && [ -r romfs ]; then
   ROMFSOPTS="-load 0x220000:romfs -rom-bank 0x22"
 fi
 exec ${EXEBIN} -load 0x200000:forth -rom-bank 0x20 ${ROMFSOPTS} -rom-file rom
