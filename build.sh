c51b2b7db73df68215245c4b24105a6175a79643
mgcaret <mgcaret@users.noreply.github.com>
2020-12-23 18:08:47 -0800

    reorganize so platforms can override the default config file

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd `dirname ${0}`
 PLATFORM=""
+ADD_OPTS=""
 if [ -n "${1}" -a -d "platforms/${1}" ]; then
   PLATFORM="${1}"
+  ADD_OPTS="${ADD_OPTS} -I platforms/${PLATFORM}/inc"
 fi
 export PLATFORM
 echo ".define PLATFORM \"${PLATFORM}\"" > platform.inc
 set -e -x
-ca65 -I inc forth.s -l forth.lst
+ca65 ${ADD_OPTS} -I inc forth.s -l forth.lst
 
